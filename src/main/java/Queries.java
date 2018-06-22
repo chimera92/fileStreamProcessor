cb5a3f0f686783396d38798cdedd60cf432381bd
Hemanth Gowda <hemanth.bm12@gmail.com>
2018-06-22 05:41:43 -0700

    Done getMeanAge.. Still to be tested.

--- src/main/java/Queries.java
+++ src/main/java/Queries.java
@@ -1,23 +1,38 @@
-import jdk.nashorn.internal.runtime.ParserException;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 class Queries
 {
 
 
     private static Map<Integer,Integer> yearVS_regUsers=new HashMap<>();
+    private static Map<Integer,Integer> allAgeCounts=new TreeMap<>();
+    private static int totalPeopleCount=0;
 
+    //Still to be tested
     public static double getMeanAge(List<Record> ipRecordList)
     {
-        return 0.0;
+        for (Record r : ipRecordList)
+        {
+            totalPeopleCount++;
+            allAgeCounts.put(r.getAge(), allAgeCounts.getOrDefault(r.getAge(), 0) + 1);
+        }
+
+        int countPointer=0;
+        int medianPos=totalPeopleCount/2;
+        for(int k:allAgeCounts.keySet())
+        {
+            countPointer+=allAgeCounts.get(k);
+            if(countPointer>medianPos)
+            {
+                System.out.println("The median age is: "+k);
+                return k;
+            }
+        }
+
+        return -1;
     }
 
 
@@ -32,7 +47,7 @@ class Queries
             int parsedDate = Integer.parseInt(r.getRegistered().substring(0,r.getRegistered().indexOf("-")));
             yearVS_regUsers.put(parsedDate,yearVS_regUsers.getOrDefault(parsedDate,0)+1);
         }
-        System.out.println(yearVS_regUsers);
+//        System.out.println(yearVS_regUsers);
         return yearVS_regUsers;
     }
 
